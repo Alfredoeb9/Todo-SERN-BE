804894fc41e5c0a199f8bb7b4bddd1a410279d14
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-11 09:54:59 -0700

    [schema] created todos schema
    created starter schema for todos tables

--- db/schema.ts
+++ db/schema.ts
@@ -2,12 +2,15 @@ import { sql } from "drizzle-orm";
 import {
   integer,
   sqliteTable,
+  sqliteTableCreator,
   text,
   uniqueIndex,
 } from "drizzle-orm/sqlite-core";
 
+export const createTable = sqliteTableCreator((name) => `todo_${name}`);
+
 // create the users schema
-export const user = sqliteTable(
+export const user = createTable(
   "users",
   {
     id: integer("id").primaryKey(),
@@ -15,5 +18,24 @@ export const user = sqliteTable(
   },
   (users) => ({
     emailIdx: uniqueIndex("emailIdx").on(users.email),
+  })
+);
+
+export const todos = createTable(
+  "todos",
+  {
+    id: integer("id").primaryKey(),
+    title: text("title").notNull(),
+    task: text("task").notNull(),
+    priority: text("priority", { enum: ["High", "Medium", "Low"] }).notNull(),
+    createdAt: integer("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: integer("updated_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+  },
+  (todos) => ({
+    nameIdx: uniqueIndex("nameIdx").on(todos.title),
   })
 );
