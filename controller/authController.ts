9d0aa3492f35b469dd8e721fd4814a63ba68060b
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-11 09:43:42 -0700

    [login] created login controller
    created login controller and emailRegex function to check if email has
    a proper format and checking if email is already in db and if it then sign in
    else create the user and return them as well

--- controller/authController.ts
+++ controller/authController.ts
@@ -1,10 +1,43 @@
 import { Request, Response } from "express";
+import { db } from "../db/db.js";
+import { user } from "../db/schema.js";
+import { eq } from "drizzle-orm";
+
+async function emailRegex(email) {
+  const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  const isValidEmail = emailRegex.test(email);
+  return isValidEmail;
+}
 
 export const login = async (req: Request, res: Response) => {
-  const { email } = req.body;
-
   try {
-    console.log(email);
-    return res.status(200).json({ message: "User logged in" });
-  } catch (error) {}
+    const { email } = req.body;
+
+    const isValidEmail = await emailRegex(email);
+
+    if (!isValidEmail) throw Error("Please provide a proper email");
+
+    // check if email is used
+    const isUserSignedUp = await db
+      .select()
+      .from(user)
+      .where(eq(user.email, email));
+
+    // check if above query not empty
+    if (isUserSignedUp.length > 0 && isUserSignedUp !== undefined) {
+      return res.status(201).json({ isUserSignedUp, message: "Logged in" });
+    }
+
+    // else we have a new user and lets put them in the database
+    const newUser = await db
+      .insert(user)
+      .values({
+        email: email,
+      })
+      .returning();
+
+    return res.status(201).json({ newUser, message: "User signed up" });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 };
