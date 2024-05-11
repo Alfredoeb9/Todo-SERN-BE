91db3427c3501b0582bae27808d8a7fcfbc5f6c3
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-11 14:04:17 -0700

    [auth] fixed authController

--- controller/authController.ts
+++ controller/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { db } from "../db/db.js";
+import { db } from "../db/index.js";
 import { user } from "../db/schema.js";
 import { eq } from "drizzle-orm";
 
