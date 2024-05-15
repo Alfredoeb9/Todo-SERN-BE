c1c376360ead2b2ea7bdeac77b568f23accc5489
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-14 18:44:26 -0700

    [clean up]

--- middleware/requireAuth.ts
+++ middleware/requireAuth.ts
@@ -18,15 +18,11 @@ export const requireAuth = async (
 ) => {
   const { authorization } = req.headers;
 
-  console.log("auth", authorization);
-
   if (!authorization) {
     return res.status(401).json({ error: "Authorization token required" });
   }
 
   const token = authorization.split(" ")[1];
-
-  console.log("toke", token);
 
   // find the user with email that matches
   req.user = await db.select().from(user).where(eq(user.email, token));
